06953ac2ebf8314e8cdb2842d4fcd395dff1e58e
Alexander Kelman <alexanderkelman95@hotmail.com>
2021-12-13 20:15:31 +0100

    Day 6 Part 1

--- AdventOfCode2021/Days/Day06.cs
+++ AdventOfCode2021/Days/Day06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,28 @@ namespace AdventOfCode2021.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File.ReadAllText(_inputPath)
+                .Split(",")
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < 80; i++)
+            {
+                var lanternfish = input.ToList();
+                for (int j = 0; j < input.Count; j++)
+                {
+                    if (lanternfish[j] == 0)
+                    {
+                        lanternfish[j] = 6;
+                        lanternfish.Add(8);
+                    }
+                    else
+                        lanternfish[j]--;
+                }
+                input = lanternfish;
+            }
+
+            return input.Count.ToString();
         }
 
         public override string SolvePart2()
