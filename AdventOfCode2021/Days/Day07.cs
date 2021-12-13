7e704f893622c4e02cb3908274e2560c954a7c90
Alexander Kelman <alexanderkelman95@hotmail.com>
2021-12-13 21:53:28 +0100

    Day 7 Part 1 & 2

--- AdventOfCode2021/Days/Day07.cs
+++ AdventOfCode2021/Days/Day07.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,56 @@ namespace AdventOfCode2021.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File
+                .ReadAllText(_inputPath)
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            var fuelCosts = new List<(int pos, int cost)>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int fuel = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    fuel += Math.Abs(input[i] - input[j]);
+                }
+                fuelCosts.Add((input[i], fuel));
+            }
+
+            return fuelCosts.OrderBy(x => x.cost).First().cost.ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var input = File
+                .ReadAllText(_inputPath)
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            var fuelCosts = new List<(int pos, int cost)>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int fuel = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    fuel += AddLower(Math.Abs(input[i] - input[j]));
+                }
+                fuelCosts.Add((input[i], fuel));
+            }
+
+            return fuelCosts.OrderBy(x => x.cost).First().cost.ToString();
+        }
+
+        private int AddLower(int value)
+        {
+            if (value <= 1) return 1;
+            return (value) + AddLower(value - 1);
         }
     }
 }
