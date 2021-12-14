3d2077d162ba0a016144cf7c7c50cc3c1515c540
Alexander Kelman <alexanderkelman95@hotmail.com>
2021-12-14 18:17:10 +0100

    Day 8 Part 1

--- AdventOfCode2021/Days/Day08.cs
+++ AdventOfCode2021/Days/Day08.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,17 @@ namespace AdventOfCode2021.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File.ReadAllLines(_inputPath)
+                .Select(x =>
+                    x.Split('|', StringSplitOptions.RemoveEmptyEntries)
+                    .Last()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Length)
+                    .Where(x => x == 2 || x == 3 || x == 4 || x == 7)
+                    .Count())
+                .Sum();
+
+            return input.ToString();
         }
 
         public override string SolvePart2()
