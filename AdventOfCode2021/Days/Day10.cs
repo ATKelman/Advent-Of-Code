e34847ee8af7f68d84cddd9bba6c6ea2f0b7d540
Alexander Kelman <alexanderkelman95@hotmail.com>
2021-12-18 16:53:52 +0100

    Day 10 Part 1

--- AdventOfCode2021/Days/Day10.cs
+++ AdventOfCode2021/Days/Day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,51 @@ namespace AdventOfCode2021.Days
             : base(day)
         { }
 
+        Dictionary<char, int> _scoreTable = new()
+        {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 }
+        };
+
+        List<(char open, char close)> _pairs = new()
+        {
+            ('(', ')'),
+            ('[', ']'),
+            ('{', '}'),
+            ('<', '>')
+        };
+
         public override string SolvePart1()
         {
-            return "";
+            var input = File
+                .ReadAllLines(_inputPath);
+
+            var illegalCharacters = new List<char>();
+            foreach (var line in input)
+            {
+                var stack = new Stack<char>();
+                foreach (var item in line)
+                {
+                    if (_pairs.Any(x => x.open == item))
+                        stack.Push(item);
+                    else
+                    {
+                        var opening = stack.Pop();
+                        if (_pairs.Any(x => x.open == opening && x.close == item))
+                            continue;
+                        else
+                            illegalCharacters.Add(item);
+                    }
+                }
+            }
+
+            return illegalCharacters
+                .GroupBy(x => x)
+                .Select(x => x.Count() * _scoreTable[x.Key])
+                .Sum()
+                .ToString();
         }
 
         public override string SolvePart2()
