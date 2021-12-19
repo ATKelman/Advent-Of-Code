2f2ff3627bd444f8fa1c48bcde7bda56410dff4a
Alexander Kelman <alexanderkelman95@hotmail.com>
2021-12-19 15:54:16 +0100

    Added Day 14

---
+++ AdventOfCode2021/Days/Day14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Days
+{
+    public class Day14 : DayBase
+    {
+        public Day14(int day)
+            : base(day)
+        { }
+
+        public override string SolvePart1()
+        {
+            return "";
+        }
+
+        public override string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2021
         {
             Console.WriteLine("Advent of Code 2021");
 
-            var day = new Day13(13);
+            var day = new Day14(14);
 
             var result = day.SolvePart1();
 
