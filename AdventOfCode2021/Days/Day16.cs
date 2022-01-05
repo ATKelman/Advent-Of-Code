ac0a66d8093a19e55c8527cc646ec40157cadfc0
Alexander Kelman <alexanderkelman95@hotmail.com>
2022-01-05 22:47:04 +0100

    Day 16 Part 2

--- AdventOfCode2021/Days/Day16.cs
+++ AdventOfCode2021/Days/Day16.cs
@@ -9,27 +9,28 @@ namespace AdventOfCode2021.Days
 {
     public class Day16 : DayBase
     {
+        public int VersionsSum { get; set; }
+        public long Sum { get; set; }
+
         public Day16(int day)
             : base(day)
-        { }
-
-        int versionsSum = 0;
-
-        public override string SolvePart1()
         {
             var input = File
                 .ReadAllText(_inputPath);
 
             var binary = StringToBinary(input);
 
-            _ = HandlePacket(binary);
-                    
-            return versionsSum.ToString();
+            Sum = HandlePacket(ref binary);
+        }
+
+        public override string SolvePart1()
+        {
+            return VersionsSum.ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            return Sum.ToString();
         }
 
         private string StringToBinary(string data)
@@ -47,62 +48,72 @@ namespace AdventOfCode2021.Days
             return sb.ToString();
         }
 
-        private string HandlePacket(string binary)
+        private long HandlePacket(ref string binary)
         {
             var version = binary.Substring(0, 3);
             binary = binary.Remove(0, 3);
 
-            versionsSum += Convert.ToInt32(version, 2);
+            VersionsSum += Convert.ToInt32(version, 2);
 
-            var type = binary.Substring(0, 3);
+            var typeBinary = binary.Substring(0, 3);
             binary = binary.Remove(0, 3);
+            var type = Convert.ToInt32(typeBinary, 2);
 
-            switch (Convert.ToInt32(type, 2))
+            if (type == 4)
             {
-                case 4:
-                    // Type = 4 Literal Value 
-                    string instance = "";
-                    StringBuilder literalValue = new();
-                    do
-                    {
-                        instance = binary.Substring(0, 5);
-                        binary = binary.Remove(0, 5);
+                // Literal Value 
+                string instance = "";
+                StringBuilder literalValue = new();
+                do
+                {
+                    instance = binary.Substring(0, 5);
+                    binary = binary.Remove(0, 5);
 
-                        literalValue.Append(instance.Substring(1, 4));
-                    } while (instance.StartsWith('1'));
+                    literalValue.Append(instance.Substring(1, 4));
+                } while (instance.StartsWith('1'));
 
-                    break;
-                default:
-                    var id = binary.Substring(0, 1);
-                    binary = binary.Remove(0, 1);
-
-                    if (id == "0")
-                    {
-                        var length = 15;
-                        var subPacketsLength = binary.Substring(0, length);
-                        binary = binary.Remove(0, length);
-
-                        var subPacketsBinaryLength = Convert.ToInt32(subPacketsLength, 2);
-                        var subPacketsBinary = binary.Substring(0, subPacketsBinaryLength);
-                        binary = binary.Remove(0, subPacketsBinaryLength);
-
-                        while (subPacketsBinary.Length >= 11)
-                            subPacketsBinary = HandlePacket(subPacketsBinary);
-                    }
-                    else
-                    {
-                        var amountOfSubpackets = binary.Substring(0, 11);
-                        binary = binary.Remove(0, 11);
-                        for (int i = 0; i < Convert.ToInt32(amountOfSubpackets, 2); i++)
-                        {
-                            binary = HandlePacket(binary);
-                        }                       
-                    }
-
-                    break;
+                return Convert.ToInt64(literalValue.ToString(), 2);
             }
+            else
+            {
+                List<long> subPacketValues = new();
 
-            return binary;
+                var id = binary.Substring(0, 1);
+                binary = binary.Remove(0, 1);
+
+                if (id == "0")
+                {
+                    var length = 15;
+                    var subPacketsLength = binary.Substring(0, length);
+                    binary = binary.Remove(0, length);
+
+                    var subPacketsBinaryLength = Convert.ToInt32(subPacketsLength, 2);
+                    var subPacketsBinary = binary.Substring(0, subPacketsBinaryLength);
+                    binary = binary.Remove(0, subPacketsBinaryLength);
+
+                    while (subPacketsBinary.Length >= 11)
+                        subPacketValues.Add(HandlePacket(ref subPacketsBinary));
+                }
+                else
+                {
+                    var amountOfSubpackets = binary.Substring(0, 11);
+                    binary = binary.Remove(0, 11);
+                    for (int i = 0; i < Convert.ToInt32(amountOfSubpackets, 2); i++)
+                        subPacketValues.Add(HandlePacket(ref binary));
+                }
+
+                return type switch
+                {
+                    0 => subPacketValues.Sum(),
+                    1 => subPacketValues.Aggregate((current, next) => current * next),
+                    2 => subPacketValues.Min(),
+                    3 => subPacketValues.Max(),
+                    5 => (subPacketValues[0] > subPacketValues[1]) ? 1 : 0,
+                    6 => (subPacketValues[0] < subPacketValues[1]) ? 1 : 0,
+                    7 => (subPacketValues[0] == subPacketValues[1]) ? 1 : 0,
+                    _ => throw new NotImplementedException($"Case not implemented for type [{type}]"),
+                };
+            }
         }
     }
 }
