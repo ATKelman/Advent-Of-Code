ce691c3f027864dbb317a3acd88506bad9e0cab0
Alexander Kelman <alexanderkelman95@hotmail.com>
2022-12-01 08:07:13 +0100

    Added AdventOfCode2022 project.
    Added Day01, DayBase and IDay classes

---
+++ AdventOfCode2022/Days/DayBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Days
+{
+    public abstract class DayBase : IDay
+    {
+        protected string _inputPath = "";
+
+        public DayBase(int day)
+        {
+            var binDir = Environment.CurrentDirectory;
+            _inputPath = $"{Directory.GetParent(binDir).Parent.Parent.FullName}\\Inputs\\Day{day}.txt";
+        }
+
+        public abstract string SolvePart1();
+        public abstract string SolvePart2();
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2022.Days;
+using System;
+
+namespace AdventOfCode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2021");
+
+            var day = new Day01(1);
+
+            var result = day.SolvePart1();
+
+            Console.WriteLine(result);
+
+            var result2 = day.SolvePart2();
+
+            Console.WriteLine(result2);
+
+            Console.WriteLine("Program Done,  Press Any Key to Exit.");
+
+            Console.ReadKey();
+        }
+    }
+}
