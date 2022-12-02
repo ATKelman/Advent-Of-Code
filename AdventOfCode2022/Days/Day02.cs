3f4b279742fb0d5ac655428416848ef5346f626e
Alexander Kelman <alexanderkelman95@hotmail.com>
2022-12-02 17:32:18 +0100

    DAy 2 Part 2

--- AdventOfCode2022/Days/Day02.cs
+++ AdventOfCode2022/Days/Day02.cs
@@ -20,15 +20,13 @@ namespace AdventOfCode2022.Days
                 .ToList()
                 .ForEach(x =>
                 {
-                    var row = x.Split(' ');
-                    var theirPick = row[0][0] - 64;
-                    var myPick = row[1][0] - 87;
+                    var theirPick = x[0] - 64;
+                    var myPick = x[2] - 87;
                     result += myPick;
 
                     if (theirPick == myPick)
                         result += 3;
-
-                    if ((theirPick + 1) % 3 == (myPick + 3) % 3)
+                    else if ((theirPick + 1) % 3 == (myPick + 3) % 3)
                         result += 6;
                 });
 
@@ -37,7 +35,22 @@ namespace AdventOfCode2022.Days
 
         public override string SolvePart2()
         {
-            return "":
+            int result = 0;
+            File.ReadAllLines(_inputPath)
+                .ToList()
+                .ForEach(x =>
+                {
+                    var gameResult = x[2] - 87;
+                    // X = 0 (Lose), Y = 3 (Draw), Z = 6 (Win)
+                    result += ((gameResult + 2) % 3) * 3;
+                    var theirPickValue = x[0] - 64;
+                    // x[2] - 87 to get the correct decimal value
+                    // + 1 then so that X % 3 = 2, Y % 3 = 0, Z % 3 = 1
+                    var myPick = (theirPickValue + ((x[2] - 87 + 1) % 3));
+                    result += (myPick > 3) ? myPick - 3 : myPick;
+                });
+
+            return result.ToString();
         }
     }
 }
