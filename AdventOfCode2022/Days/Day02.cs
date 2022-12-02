8c43790293a8dc5bd46b83c53c8dd6237c5b67cc
Alexander Kelman <alexanderkelman95@hotmail.com>
2022-12-02 08:32:37 +0100

    Day 2 Part 1

---
+++ AdventOfCode2022/Days/Day02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Days
+{
+    class Day02 : DayBase
+    {
+        public Day02(int day)
+        : base(day)
+        { }
+
+        public override string SolvePart1()
+        {
+            int result = 0;
+            File.ReadAllLines(_inputPath)
+                .ToList()
+                .ForEach(x =>
+                {
+                    var row = x.Split(' ');
+                    var theirPick = row[0][0] - 64;
+                    var myPick = row[1][0] - 87;
+                    result += myPick;
+
+                    if (theirPick == myPick)
+                        result += 3;
+
+                    if ((theirPick + 1) % 3 == (myPick + 3) % 3)
+                        result += 6;
+                });
+
+            return result.ToString();
+        }
+
+        public override string SolvePart2()
+        {
+            return "":
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2022
         {
             Console.WriteLine("Advent of Code 2022");
 
-            var day = new Day01(1);
+            var day = new Day02(2);
 
             var result = day.SolvePart1();
 
