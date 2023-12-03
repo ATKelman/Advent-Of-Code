d90e05d9117585d588fe195c01c03365dbbba6b3
Alexander Kelman <alexanderkelman95@outlook.com>
2023-12-03 21:45:52 +0100

    Day 4 preparation

---
+++ AdventOfCode2023/Days/Day4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Days;
+public class Day4 : DayBase
+{
+    public Day4(IConfiguration config, bool isTest)
+        : base(config, 2023, 4, isTest)
+    {
+            
+    }
+
+    public override string SolvePart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
