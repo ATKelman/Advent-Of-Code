85e665ed5d763732519207306500b9722d867a3c
Alexander Kelman <alexanderkelman95@outlook.com>
2023-12-09 22:18:02 +0100

    Day 6 Part 1 & 2

--- AdventOfCode2023/Days/Day6.cs
+++ AdventOfCode2023/Days/Day6.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2023.Days;
@@ -16,11 +17,62 @@ public class Day6 : DayBase
 
     public override string SolvePart1()
     {
-        throw new NotImplementedException();
+        Regex regex = new Regex(@"\d+");
+        var input = File.ReadAllLines(_inputPath)
+            .Select(x =>
+                regex.Matches(x)
+                .Select(y => int.Parse(y.Value)).ToArray())
+            .ToArray();
+
+        long winningOptions = 1;
+
+        for (int i = 0; i < input[0].Count(); i++)
+        {
+            var time = input[0][i];
+            var record = input[1][i];
+
+            var lower = (-time - Math.Sqrt(time * time - 4 * record)) / 2;
+            var h1 = (long)Math.Ceiling(lower);
+            if (h1 == lower)
+                h1++;
+
+            var upper = (-time + Math.Sqrt(time * time - 4 * record)) / 2;
+            var h2 = (long)Math.Floor(upper);
+            if (h2 == upper)
+                h2--;
+
+            winningOptions *= (h2 - h1 + 1);
+        }
+
+        return winningOptions.ToString();
     }
 
     public override string SolvePart2()
     {
-        throw new NotImplementedException();
+        Regex regex = new Regex(@"\d+");
+        var input = File.ReadAllLines(_inputPath)
+            .Select(x =>
+                regex.Matches(x)
+                .Aggregate("", (tot, val) => $"{tot}{val.Value}"))
+            .ToArray();
+
+        var time = long.Parse(input[0]);
+        var record = long.Parse(input[1]);
+
+        var lower = (-time - Math.Sqrt(time * time - 4 * record)) / 2;
+        var upper = (-time + Math.Sqrt(time * time - 4 * record)) / 2;
+
+        var h1 = (long)Math.Ceiling(lower);
+        if (h1 == lower)
+            h1++;
+
+            
+        var h2 = (long)Math.Floor(upper);
+        if (h2 == upper)
+            h2--;
+
+        long winningOptions = (h2 - h1 + 1);
+
+        return winningOptions.ToString();
     }
 }
