fd6f7d5d53ba6b25a79f850de446d4917b30fd0f
Alexander Kelman <alexanderkelman95@outlook.com>
2023-12-10 21:09:16 +0100

    Day 9 Part 1 & 2

--- AdventOfCode2023/Days/Day9.cs
+++ AdventOfCode2023/Days/Day9.cs
@@ -16,11 +16,38 @@ public class Day9 : DayBase
 
     public override string SolvePart1()
     {
-        throw new NotImplementedException();
+        var input = File.ReadAllLines(_inputPath)
+            .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList())
+            .Select(GetHistory)
+            .ToList()
+            .Select(x => x.Last())
+            .Sum();
+
+        return input.ToString();
+    }
+
+    private List<long> GetHistory(List<long> sequence)
+    {
+        if (sequence.All(x => x == 0))
+            return sequence;
+        List<long> newPairs = new();
+        for (var i = 0; i < sequence.Count() - 1; i++)
+            newPairs.Add(sequence[i + 1] - sequence[i]);
+
+        var nextSequence = GetHistory(newPairs);
+        sequence.Add(nextSequence.Last() + sequence.Last());
+        return sequence;
     }
 
     public override string SolvePart2()
     {
-        throw new NotImplementedException();
+        var input = File.ReadAllLines(_inputPath)
+            .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).Reverse().ToList())
+            .Select(GetHistory)
+            .ToList()
+            .Select(x => x.Last())
+            .Sum();
+
+        return input.ToString();
     }
 }
