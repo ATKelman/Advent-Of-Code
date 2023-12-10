d77fc4f7d919e814c06e378bae8fe9470711d4ae
Alexander Kelman <alexanderkelman95@outlook.com>
2023-12-10 20:32:50 +0100

    Day 8 Part 1 & 2

--- AdventOfCode2023/Days/Day8.cs
+++ AdventOfCode2023/Days/Day8.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2023.Days;
@@ -15,11 +16,62 @@ public class Day8 : DayBase
     }
     public override string SolvePart1()
     {
-        throw new NotImplementedException();
+        var input = File.ReadAllText(_inputPath)
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var instructions = input[0].ToCharArray();
+        Regex regex = new(@"[A-Z]+");
+        var nodes = input
+            .Skip(1)
+            .Select(x => regex.Matches(x).ToArray())
+            .ToDictionary(x => x[0].Value, x => (left: x[1].Value, right: x[2].Value));
+
+        return GetSteps(instructions, nodes, "AAA", "ZZZ").ToString();
     }
 
     public override string SolvePart2()
     {
-        throw new NotImplementedException();
+        var input = File.ReadAllText(_inputPath)
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var instructions = input[0].ToCharArray();
+        Regex regex = new(@"[A-Z]+");
+        var nodes = input
+            .Skip(1)
+            .Select(x => regex.Matches(x).ToArray())
+            .ToDictionary(x => x[0].Value, x => (left: x[1].Value, right: x[2].Value));
+
+        var result = nodes.Keys.Where(x => x[2] == 'A')
+            .Select(x => GetSteps(instructions, nodes, x, "Z"))
+            .Aggregate(LCM);
+
+        return result.ToString();
+    }
+
+    private long GetSteps(char[] instructions, Dictionary<string, (string left, string right)> nodes, string node, string ending)
+    {
+        long steps = 0;
+        while (!node.EndsWith(ending))
+        {
+            node = instructions[steps % instructions.Length] == 'L'
+                ? nodes[node].left
+                : nodes[node].right;
+            steps++;
+        }
+        return steps;
+    }
+
+    // Googled LCM implementation C# and found this result. 
+    private long LCM(long a, long b) => (a / GCF(a, b)) * b;
+
+    private long GCF(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
